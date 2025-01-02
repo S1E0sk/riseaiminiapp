e1079d069c6cafcd40825c2794ff1c18a2fa5a96
S1E0 <110777245+S1E0sk@users.noreply.github.com>
2025-01-03 00:26:33 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,10 +1,8 @@
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup, WebAppInfo
 from telegram.ext import Application, CommandHandler
 
-# Bot token you get from BotFather
 TOKEN = "8017242797:AAE4pGGObZ8KJnUdyne3sdZ6MVh_aLm5OII"
 
-# /start command function
 async def start(update, context):
     keyboard = [
         [InlineKeyboardButton("Mini App Başlat", web_app=WebAppInfo(url="https://riseai-theta.vercel.app/"))]
@@ -12,15 +10,11 @@ async def start(update, context):
     reply_markup = InlineKeyboardMarkup(keyboard)
     await update.message.reply_text("Mini App'ı başlatmak için tıklayın:", reply_markup=reply_markup)
 
-# Main function to run the bot
 def main():
-    # Create the application using the bot token
     application = Application.builder().token(TOKEN).build()
 
-    # Add handler for the /start command
     application.add_handler(CommandHandler("start", start))
 
-    # Run the bot with async methods
     application.run_polling()
 
 if __name__ == '__main__':
